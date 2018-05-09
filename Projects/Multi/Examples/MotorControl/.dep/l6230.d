307d1307c27dae05b3075392e6dc5ab1f55d6b2f
Hawk Kim <hawk_kim74@hotmail.com>
2018-05-09 20:43:39 +0900

    first commit

---
+++ Projects/Multi/Examples/MotorControl/.dep/l6230.d
@@ -0,0 +1,3 @@
+build/l6230.o: ../../../../Drivers/BSP/Components/l6230/l6230.c \
+ ../../../../Drivers/BSP/Components/l6230/l6230.h \
+ ../../../../Drivers/BSP/Components/Common/MC_Common.h
---
+++ README.md
@@ -0,0 +1,1 @@
+# stm32f401re_motor
